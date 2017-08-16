5d4a03808bcd882d449646e2ccb8a29576dcc203
Alfonso Angeles <alphonse88@gmail.com>
2017-08-16 08:27:19 -0700

    Added required actors, messages and started configuring the Web SPA

---
+++ Game.Web/Models/GameActorSystem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Game.ActorModel.Actors;
+
+namespace Game.Web.Models
+{
+    public static class GameActorSystem
+    {
+        private static ActorSystem ActorSystem;
+
+        public static void Create()
+        {
+            ActorSystem = Akka.Actor.ActorSystem.Create("GameSystem");
+
+            ActorReferences.GameController = ActorSystem.ActorOf<GameControllerActor>();
+        }
+
+        public static void Shutdown()
+        {
+            AsyncShutdown().Wait(TimeSpan.FromSeconds(1));
+        }
+
+        private static async Task AsyncShutdown()
+        { 
+            await ActorSystem.WhenTerminated;
+        }
+
+        public static class ActorReferences
+        {
+            public static IActorRef GameController { get; set; }
+        }
+    }
+}
