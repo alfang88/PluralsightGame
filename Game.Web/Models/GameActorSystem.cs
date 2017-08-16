78fe38ed928160994799910cc065311a8c7437ac
Alfonso Angeles <alphonse88@gmail.com>
2017-08-16 10:45:38 -0700

    Updated jQuery according to course specifications. Implemented SignalRBridge (backend)

--- Game.Web/Models/GameActorSystem.cs
+++ Game.Web/Models/GameActorSystem.cs
@@ -2,18 +2,27 @@
 using System.Threading.Tasks;
 using Akka.Actor;
 using Game.ActorModel.Actors;
+using Game.ActorModel.ExternalSystems;
 
 namespace Game.Web.Models
 {
     public static class GameActorSystem
     {
-        private static ActorSystem ActorSystem;
+        private static ActorSystem _actorSystem;
+        private static IGameEventsPusher _gameEventsPusher;
 
         public static void Create()
         {
-            ActorSystem = Akka.Actor.ActorSystem.Create("GameSystem");
+            _gameEventsPusher = new SignalRGameEventPusher();
 
-            ActorReferences.GameController = ActorSystem.ActorOf<GameControllerActor>();
+            _actorSystem = Akka.Actor.ActorSystem.Create("GameSystem");
+
+            ActorReferences.GameController = _actorSystem.ActorOf<GameControllerActor>();
+
+            ActorReferences.SignalRBridge = _actorSystem.ActorOf(
+                Props.Create(() => new SignalRBridgeActor(_gameEventsPusher, ActorReferences.GameController)),
+                "SignalRBridge"
+                );
         }
 
         public static void Shutdown()
@@ -23,12 +32,14 @@ namespace Game.Web.Models
 
         private static async Task AsyncShutdown()
         { 
-            await ActorSystem.WhenTerminated;
+            await _actorSystem.WhenTerminated;
+            Console.WriteLine("ActorSystem terminated...");
         }
 
         public static class ActorReferences
         {
             public static IActorRef GameController { get; set; }
+            public static IActorRef SignalRBridge { get; set; }
         }
     }
 }
